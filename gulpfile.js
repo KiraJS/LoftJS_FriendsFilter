51d295504e983eee8b0d3991f5d6ca0cb6fe3913
Kira Shatulova <kira.shatulova@gmail.com>
2016-03-31 17:13:54 +0300

    markup & style

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var webserver = require('gulp-webserver');
+
+//sass
+gulp.task('scss', function (){
+  return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(notify('Done!'));
+})
+
+//webserver
+gulp.task('webserver', function(){
+  gulp.src('')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      port: 8034,
+    }));
+})
+
+//watch
+gulp.task('watch', function () {
+  gulp.watch('**/*.js', ['js']);
+  gulp.watch('**/*.scss', ['scss']);
+})
+
+
+//default
+gulp.task('default', ['scss', 'watch', 'webserver'])
